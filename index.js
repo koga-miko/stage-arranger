de915452a79be6d88db729d6fb7e8763959d65ac
Yoshiharu Hamaguchi <yoshihama0141@gmail.com>
2022-10-15 12:40:55 +0900

    Support for displaying applications by starting only the server.

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const PORT = 5000;
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+if (!process.env.PORT) {
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+    })
+  );
+}
 app.use(express.json());
 
 const routes = require("express").Router();
 app.use("/api/v1", routes);
 routes.use("/auth", require("./src/v1/routes/auth"));
 routes.use("/record", require("./src/v1/routes/record"));
+app.use(
+  "/static",
+  express.static(path.join(__dirname, "./frontend/build/static"))
+);
+app.get("/logo192.png", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/build/logo192.png"));
+});
+app.get("/logo512.png", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/build/logo512.png"));
+});
+app.get("/favicon.ico", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/build/favicon.ico"));
+});
+app.get("/manifest.json", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/build/manifest.json"));
+});
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+});
 
 //DB接続
 try {
@@ -26,5 +48,5 @@ try {
 }
 
 app.listen(PORT, () => {
-  console.log("ローカルサーバー起動中・・・");
+  console.log(`ローカルサーバー起動中・・・PORT=${PORT}`);
 });
