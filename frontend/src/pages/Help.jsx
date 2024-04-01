bf7a3609cf0325b600207c4d7999787110a2f15d
Yoshiharu Hamaguchi <yoshihama0141@gmail.com>
2024-04-02 00:40:19 +0900

    Revert "modified the help layout for fixing the bug."
    This reverts commit 54d470a910b72381e606edb26636f83ff50adac0.

--- frontend/src/pages/Help.jsx
+++ frontend/src/pages/Help.jsx
@@ -7,7 +7,6 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-import "./Help.css";
 
 function createData(target, operation, behavior) {
   return { target, operation, behavior };
