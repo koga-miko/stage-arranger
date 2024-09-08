00cab813909f0a91d7eaeca8641cee04b6758196
Yoshiharu Hamaguchi <yoshihama0141@gmail.com>
2024-09-08 16:53:40 +0900

    modify for local server 8888

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -3,7 +3,9 @@ import axios from "axios";
 let server_url = "http://localhost:5000/api/v1/";
 if (process.env.NODE_ENV === "production") {
   //  server_url = "https://stage-arranger.herokuapp.com/api/v1/"; // for Heroku
-  server_url = "http://localhost:8888/api/v1/"; // for local-server
+  // server_url = "http://localhost:8888/api/v1/"; // for local-server
+  server_url =
+    "https://27ea-2400-2650-2222-ce00-9df4-aee7-6e99-eb08.ngrok-free.app/api/v1/"; // for local-server via ngrok
 }
 const getToken = () => localStorage.getItem("token");
 const axiosClient = axios.create({
