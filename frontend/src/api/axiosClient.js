1afaf711f6bbaa7963154d96d1d825f98f51c5e9
koga-miko <yoshihama0141@gmail.com>
2024-09-08 22:12:07 +0900

    modify server path for rok setting

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === "production") {
   //  server_url = "https://stage-arranger.herokuapp.com/api/v1/"; // for Heroku
   // server_url = "http://localhost:8888/api/v1/"; // for local-server
   server_url =
-    "https://27ea-2400-2650-2222-ce00-9df4-aee7-6e99-eb08.ngrok-free.app/api/v1/"; // for local-server via ngrok
+    "https://af89-2400-2650-2222-ce00-1601-6c95-d0a1-ef89.ngrok-free.app/api/v1/"; // for local-server via ngrok
 }
 const getToken = () => localStorage.getItem("token");
 const axiosClient = axios.create({
