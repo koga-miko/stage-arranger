9aaecc89ca8574958ddea9f607df0c98d3d429a4
Yoshiharu Hamaguchi <yoshihama0141@gmail.com>
2025-05-03 22:16:17 +0900

    modify the bug that port number is incorrect.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Change the current working directory to the directory of this file
+process.chdir(__dirname);
+console.log(`Current working directory: ${process.cwd()}`);
+
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -5,10 +9,6 @@ const path = require("path");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
-
-// Change the current working directory to the directory of this file
-process.chdir(__dirname);
-console.log(`Current working directory: ${process.cwd()}`);
 
 if (!process.env.PORT) {
   app.use(
